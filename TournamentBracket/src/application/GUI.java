f58bafe74ec1496a8f157036938acbff06ad360c
Chentao Wang <cwang556@wisc.edu>
2018-04-20 12:25:29 -0500

    update ui

--- TournamentBracket/src/application/GUI.java
+++ TournamentBracket/src/application/GUI.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -10,6 +11,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -30,188 +33,200 @@ import javafx.stage.Stage;
 
 public class GUI {
 
-	public static BorderPane setupGUI(BorderPane root) {
-		Main.gameCount = 0;
+    public static BorderPane setupGUI(BorderPane root) {
+        Main.gameCount = 0;
+        BorderPane bp = new BorderPane();
+        root.setBottom(bp);     
+        Main.left = new VBox();
+        Main.left.getStyleClass().add("vbox-left");
+        Main.center = new HBox();
+        Main.center.getStyleClass().add("hbox-big");
+        preComputationLeft();
+        root.setLeft(Main.left);
+        Main.preComputationRanking();
+        Main.setGames();
+        preComputationCenter();
+        root.setCenter(Main.center);
+        return root;
+    }
 
-		Main.left = new VBox();
-		Main.center = new HBox();
+    private static void preComputationLeft() {
+        Button loadFile = new Button();
+        loadFile = setupLoadFile(loadFile);
+        loadFile.getStyleClass().add("button-load");
+        Main.left.getChildren().add(loadFile);
 
-		preComputationLeft();
-		root.setLeft(Main.left);
+        VBox teamList = new VBox();
+        teamList = setupTeamList(teamList);
+        Main.left.getChildren().add(teamList);
+    }
 
-		Main.preComputationRanking();
+    private static VBox setupTeamList(VBox teamList) {
+        Label labelHead = new Label();
+        labelHead.setText("League Rank");
+        labelHead.setFont(new Font("Algerian", 15));
+        teamList.getChildren().add(labelHead);
+        for (Team team : Main.teams) {
+            Label label = new Label();
+            label.getStyleClass().add("label-names");
+            label.setText(team.getRank() + "  " + team.getName());
+            label.setFont(new Font("Algerian", 13));
+            teamList.getChildren().add(label);
+        }
+        return teamList;
+    }
 
-		preComputationCenter();
-		root.setCenter(Main.center);
+    private static Button setupLoadFile(Button loadFile) {
+        loadFile.setText("Load");
+        loadFile.setFont(new Font("Algerian", 13));
+        loadFile.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open Resource File");
+                fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+                        new ExtensionFilter("All Files", "*.*"));
+                File selectedFile = fileChooser.showOpenDialog(Main.stage);
+                if (selectedFile != null) {
+                    Main.teams.clear();
+                    Main.readFile(null, selectedFile);
+                    Main.root = GUI.setupGUI(Main.root);
+                }
+            }
+        });
+        return loadFile;
+    }
 
-		return root;
-	}
+    private static void preComputationCenter() {
+        Main.currentPage = new HBox();
+        if (Main.teams.size() > 1) {
+            Main.currentPage = setupPageRegular();
+        } else {
 
-	private static void preComputationLeft() {
-		Button loadFile = new Button();
-		loadFile = setupLoadFile(loadFile);
-		Main.left.getChildren().add(loadFile);
+        }
+        Main.center.getChildren().add(Main.currentPage);
+    }
 
-		VBox teamList = new VBox();
-		teamList = setupTeamList(teamList);
-		Main.left.getChildren().add(teamList);
-	}
+    private static HBox setupPageRegular() {
+        ArrayList<VBox> vBoxList = new ArrayList<>();
+        int vBoxNum = (int)(Math.log10(Main.teams.size())/Math.log10(2)) * 2 - 1;
+        int index = 0;
 
-	private static VBox setupTeamList(VBox teamList) {
-		Label labelHead = new Label();
-		labelHead.setText("League Rank");
-		labelHead.setFont(new Font("Algerian", 15));
-		teamList.getChildren().add(labelHead);
-		for (Team team : Main.teams) {
-			Label label = new Label();
-			label.setText(team.getRank() + "  " + team.getName());
-			label.setFont(new Font("Algerian", 13));
-			teamList.getChildren().add(label);
-		}
-		return teamList;
-	}
+        for (int i = 0; i <vBoxNum; i++) {
+            vBoxList.add(new VBox());
+        }
+        VBox temp1 = Main.games.get(Main.games.size() - 1).setUpGame();
+        temp1.getStyleClass().add("vbox-small");
+        vBoxList.get(vBoxNum/2).getChildren().add(temp1);
+        vBoxList.get(vBoxNum/2).getStyleClass().add("vbox-big");
+        for (int i = 0; i < vBoxNum / 2; i++) {
+            for (int j = 0; j < (int) Math.pow(2, vBoxNum / 2 - 1 - i); j++) {
+                VBox temp2 = Main.games.get(index).setUpGame();
+                temp2.getStyleClass().add("vbox-small");
+                vBoxList.get(i).getChildren().add(temp2);
+                vBoxList.get(i).getStyleClass().add("vbox-big");
+                index++;
+            }
+            for (int j = 0; j < (int) Math.pow(2, vBoxNum / 2 - 1 - i); j++) {
+                VBox temp3 = Main.games.get(index).setUpGame(); 
+                temp3.getStyleClass().add("vbox-small");
+                vBoxList.get(vBoxList.size() - i - 1).getChildren().add(temp3);
+                vBoxList.get(vBoxList.size() - i - 1).getStyleClass().add("vbox-big");
+                index++;
+            }
+        }
+           
+//      Button submit = new Button();
+//      submit.setText("Submit Scores");
+//      submit.setFont(new Font("Algerian", 11));
+//      submit.setOnAction(new EventHandler<ActionEvent>() {
+//          @Override
+//          public void handle(ActionEvent event) {
+//              progressRegular();
+//          }
+//      });
+        
+        HBox page = new HBox();
+        for (int i = 0; i < vBoxList.size(); i++)
+            page.getChildren().add(vBoxList.get(i));
+        return page;
+    }
 
-	private static Button setupLoadFile(Button loadFile) {
-		loadFile.setText("Load");
-		loadFile.setFont(new Font("Algerian", 13));
-		loadFile.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				FileChooser fileChooser = new FileChooser();
-				fileChooser.setTitle("Open Resource File");
-				fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
-						new ExtensionFilter("All Files", "*.*"));
-				File selectedFile = fileChooser.showOpenDialog(Main.stage);
-				if (selectedFile != null) {
-					Main.teams.clear();
-					Main.readFile(null, selectedFile);
-					Main.root = GUI.setupGUI(Main.root);
-				}
-			}
-		});
-		return loadFile;
-	}
+//  private static HBox setupOneTeam(HBox oneTeam, Team team) {
+//      Label teamProperty = new Label();
+//      teamProperty.setFont(new Font("Algerian", 12));
+//      teamProperty.setText(team.getRank() + "  " + team.getName());
+//
+//      TextField score = new TextField();
+//      score.setMaxWidth(50);
+//      score.setFont(new Font("Algerian", 12));
+//      score.setPromptText("Score");
+//
+//      oneTeam.getChildren().add(teamProperty);
+//      oneTeam.getChildren().add(score);
+//      return oneTeam;
+//  }
 
-	private static void preComputationCenter() {
-		Main.currentPage = new HBox();
-		if (Main.teams.size() > 1) {
-			Main.currentPage = setupPageRegular(Main.currentPage);
-		} else {
+//  private static void progressRegular() {
+//      if (saveCurrentScores() == -1)
+//          return;
+//      Main.computeWinners();
+//      generateNewPage();
+//  }
 
-		}
-		Main.center.getChildren().add(Main.currentPage);
-	}
+//  private static void generateNewPage() {
+//      HBox root = (HBox) Main.center.getChildren().get(0);
+//      for (int i = 0; i < Main.gameCount; i++)
+//          root = (HBox) root.getChildren().get(1);
+//      Main.gameCount++;
+//      Main.currentPage = new HBox();
+//      Main.currentPage = setupPageRegular(Main.currentPage);
+//      root.getChildren().set(1, Main.currentPage);
+//  }
 
-	private static HBox setupPageRegular(HBox page) {
-		VBox leftSide = new VBox(30);
-		VBox rightSide = new VBox(30);
+    private static int saveCurrentScores() {
+        VBox leftSide = (VBox) Main.currentPage.getChildren().get(0);
+        for (int i = 0; i < leftSide.getChildren().size(); i++) {
+            try {
+                int score = Integer.parseInt(
+                        ((TextField) (((HBox) (leftSide).getChildren().get(i)).getChildren().get(1))).getText());
+                Main.teams.get(i).setGameScore(score);
+                ((TextField) (((HBox) (leftSide).getChildren().get(i)).getChildren().get(1))).setEditable(false);
+            } catch (NumberFormatException e) {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setHeaderText("The scores entered have illegal content!");
+                alert.setContentText("Please enter scores as integers.\n");
+                alert.showAndWait();
+                return -1;
+            }
+        }
 
-		for (int i = 0; i < Main.teams.size() / 2; i++) {
-			HBox oneTeam = new HBox();
-			oneTeam = setupOneTeam(oneTeam, Main.teams.get(i));
-			leftSide.getChildren().add(oneTeam);
-		}
+        VBox rightSide = (VBox) Main.currentPage.getChildren().get(2);
+        for (int i = 0; i < rightSide.getChildren().size(); i++) {
+            try {
+                int score = Integer.parseInt(
+                        ((TextField) (((HBox) (rightSide).getChildren().get(i)).getChildren().get(1))).getText());
+                Main.teams.get(rightSide.getChildren().size() + i).setGameScore(score);
+            } catch (NumberFormatException e) {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setHeaderText("The scores entered have illegal content!");
+                alert.setContentText("Please enter scores as integers.\n");
+                alert.showAndWait();
+                return -1;
+            }
+        }
 
-		for (int i = Main.teams.size() / 2; i < Main.teams.size(); i++) {
-			HBox oneTeam = new HBox();
-			oneTeam = setupOneTeam(oneTeam, Main.teams.get(i));
-			rightSide.getChildren().add(oneTeam);
-		}
+        for (int i = 0; i < leftSide.getChildren().size(); i++)
+            ((TextField) (((HBox) (leftSide).getChildren().get(i)).getChildren().get(1))).setEditable(false);
+        for (int i = 0; i < rightSide.getChildren().size(); i++)
+            ((TextField) (((HBox) (rightSide).getChildren().get(i)).getChildren().get(1))).setEditable(false);
 
-		Button submit = new Button();
-		submit.setText("Submit Scores");
-		submit.setFont(new Font("Algerian", 11));
-		submit.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				progressRegular();
-			}
-		});
+        return 0;
+    }
 
-		HBox center = new HBox();
-		center.getChildren().add(submit);
+    private static void winningTeam() {
 
-		page.getChildren().add(leftSide);
-		page.getChildren().add(center);
-		page.getChildren().add(rightSide);
-		return page;
-	}
-
-	private static HBox setupOneTeam(HBox oneTeam, Team team) {
-		Label teamProperty = new Label();
-		teamProperty.setFont(new Font("Algerian", 12));
-		teamProperty.setText(team.getRank() + "  " + team.getName());
-
-		TextField score = new TextField();
-		score.setMaxWidth(50);
-		score.setFont(new Font("Algerian", 12));
-		score.setPromptText("Score");
-
-		oneTeam.getChildren().add(teamProperty);
-		oneTeam.getChildren().add(score);
-		return oneTeam;
-	}
-
-	private static void progressRegular() {
-		if (saveCurrentScores() == -1)
-			return;
-		Main.computeWinners();
-		generateNewPage();
-	}
-
-	private static void generateNewPage() {
-		HBox root = (HBox) Main.center.getChildren().get(0);
-		for (int i = 0; i < Main.gameCount; i++)
-			root = (HBox) root.getChildren().get(1);
-		Main.gameCount++;
-		Main.currentPage = new HBox();
-		Main.currentPage = setupPageRegular(Main.currentPage);
-		root.getChildren().set(1, Main.currentPage);
-	}
-
-	private static int saveCurrentScores() {
-		VBox leftSide = (VBox) Main.currentPage.getChildren().get(0);
-		for (int i = 0; i < leftSide.getChildren().size(); i++) {
-			try {
-				int score = Integer.parseInt(
-						((TextField) (((HBox) (leftSide).getChildren().get(i)).getChildren().get(1))).getText());
-				Main.teams.get(i).setGameScore(score);
-				((TextField) (((HBox) (leftSide).getChildren().get(i)).getChildren().get(1))).setEditable(false);
-			} catch (NumberFormatException e) {
-				Alert alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Error Dialog");
-				alert.setHeaderText("The scores entered have illegal content!");
-				alert.setContentText("Please enter scores as integers.\n");
-				alert.showAndWait();
-				return -1;
-			}
-		}
-
-		VBox rightSide = (VBox) Main.currentPage.getChildren().get(2);
-		for (int i = 0; i < rightSide.getChildren().size(); i++) {
-			try {
-				int score = Integer.parseInt(
-						((TextField) (((HBox) (rightSide).getChildren().get(i)).getChildren().get(1))).getText());
-				Main.teams.get(rightSide.getChildren().size() + i).setGameScore(score);
-			} catch (NumberFormatException e) {
-				Alert alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Error Dialog");
-				alert.setHeaderText("The scores entered have illegal content!");
-				alert.setContentText("Please enter scores as integers.\n");
-				alert.showAndWait();
-				return -1;
-			}
-		}
-
-		for (int i = 0; i < leftSide.getChildren().size(); i++)
-			((TextField) (((HBox) (leftSide).getChildren().get(i)).getChildren().get(1))).setEditable(false);
-		for (int i = 0; i < rightSide.getChildren().size(); i++)
-			((TextField) (((HBox) (rightSide).getChildren().get(i)).getChildren().get(1))).setEditable(false);
-
-		return 0;
-	}
-
-	private static void winningTeam() {
-
-	}
+    }
 }
--- TournamentBracket/src/application/Main.java
+++ TournamentBracket/src/application/Main.java
@@ -14,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -29,143 +30,157 @@ import javafx.scene.layout.VBox;
 ///////////////////////////////////////////////////////////////////////////////
 
 public class Main extends Application {
-	static final int HEIGHT = 1100;
-	static final int WIDTH = 700;
+    static final int HEIGHT = 1080;
+    static final int WIDTH = 720;
 
-	static ArrayList<Team> teams = new ArrayList<Team>();
-	static Integer gameCount;
-	static HBox currentPage;
+    static ArrayList<Team> teams = new ArrayList<Team>();
+    static ArrayList<Game> games = new ArrayList<Game>();
+    static Integer gameCount;
+    static HBox currentPage;
 
-	static Stage stage = new Stage();
-	static VBox left;
-	static HBox center;
-	static BorderPane root;
+    static Stage stage = new Stage();
+    static VBox left;
+    static HBox center;
+    static BorderPane root;
 
-	private static void loadDefaultFile(String path) {
-		if (path == null) {
-			readFile("src" + File.separator + "teams.txt", null);
-		} else {
-			readFile(path, null);
-		}
-	}
+    private static void loadDefaultFile(String path) {
+        if (path == null) {
+            readFile("src" + File.separator + "teams.txt", null);
+        } else {
+            readFile(path, null);
+        }
+    }
 
-	static void readFile(String fileName, File file) {
-		try {
-			if (!(fileName == null))
-				file = new File(fileName);
-			BufferedReader br;
-			br = new BufferedReader(new FileReader(file));
-			String line = br.readLine();
-			int rank = 0;
-			while (line != null) {
-				teams.add(new Team(line.trim(), ++rank));
-				line = br.readLine();
-			}
-			br.close();
-			if (!(rank == 0 || rank == 1 || rank == 2 || rank == 4 || rank == 8 || rank == 16 || rank == 32)) {
-				Alert alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Error Dialog");
-				alert.setHeaderText("The number of teams is illegal!");
-				alert.setContentText("Please select a new file that contains legal number of teams.\n"
-						+ "Accepted numbers are 0, 1, 2, 4, 8, 16, and 32.\n" + "The program will now TERMINATE.");
-				alert.showAndWait();
-				System.exit(0);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    static void readFile(String fileName, File file) {
+        try {
+            if (!(fileName == null))
+                file = new File(fileName);
+            BufferedReader br;
+            br = new BufferedReader(new FileReader(file));
+            String line = br.readLine();
+            int rank = 0;
+            while (line != null) {
+                teams.add(new Team(line.trim(), ++rank));
+                line = br.readLine();
+            }
+            br.close();
+            if (!(rank == 0 || rank == 1 || rank == 2 || rank == 4 || rank == 8 || rank == 16 || rank == 32)) {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setHeaderText("The number of teams is illegal!");
+                alert.setContentText("Please select a new file that contains legal number of teams.\n"
+                        + "Accepted numbers are 0, 1, 2, 4, 8, 16, and 32.\n" + "The program will now TERMINATE.");
+                alert.showAndWait();
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	static void preComputationRanking() {
-		int[] left = new int[teams.size() / 2];
-		int[] right = new int[teams.size() / 2];
-		ArrayList<Team> ranking = new ArrayList<Team>();
-		int indexLeft = 0;
-		int indexRight = 0;
-		for (int i = 0; i < teams.size(); i++) {
-			if ((i % 4 == 0) || (i % 4 == 3)) {
-				left[indexLeft] = i;
-				indexLeft++;
-			} else {
-				right[indexRight] = i;
-				indexRight++;
-			}
-		}
-		int i = 0;
-		int j = teams.size() / 4 - 1;
-		while (i < j) {
-			ranking.add(teams.get(left[i]));
-			ranking.add(teams.get(left[teams.size() / 2 - i - 1]));
-			ranking.add(teams.get(left[j]));
-			ranking.add(teams.get(left[teams.size() / 2 - j - 1]));
-			i++;
-			j--;
-		}
-		i = 0;
-		j = teams.size() / 4 - 1;
-		while (i < j) {
-			ranking.add(teams.get(right[i]));
-			ranking.add(teams.get(right[teams.size() / 2 - i - 1]));
-			ranking.add(teams.get(right[j]));
-			ranking.add(teams.get(right[teams.size() / 2 - j - 1]));
-			i++;
-			j--;
-		}
-		teams = ranking;
-	}
+    static void preComputationRanking() {
+        int[] left = new int[teams.size() / 2];
+        int[] right = new int[teams.size() / 2];
+        ArrayList<Team> ranking = new ArrayList<Team>();
+        int indexLeft = 0;
+        int indexRight = 0;
+        for (int i = 0; i < teams.size(); i++) {
+            if ((i % 4 == 0) || (i % 4 == 3)) {
+                left[indexLeft] = i;
+                indexLeft++;
+            } else {
+                right[indexRight] = i;
+                indexRight++;
+            }
+        }
+        int i = 0;
+        int j = teams.size() / 4 - 1;
+        while (i < j) {
+            ranking.add(teams.get(left[i]));
+            ranking.add(teams.get(left[teams.size() / 2 - i - 1]));
+            ranking.add(teams.get(left[j]));
+            ranking.add(teams.get(left[teams.size() / 2 - j - 1]));
+            i++;
+            j--;
+        }
+        i = 0;
+        j = teams.size() / 4 - 1;
+        while (i < j) {
+            ranking.add(teams.get(right[i]));
+            ranking.add(teams.get(right[teams.size() / 2 - i - 1]));
+            ranking.add(teams.get(right[j]));
+            ranking.add(teams.get(right[teams.size() / 2 - j - 1]));
+            i++;
+            j--;
+        }
+        teams = ranking;
+    }
+    
+    static void setGames() {
+        int gameIndex = 1;
+        for (int i = 0; i < teams.size() / 2; i++) {
+            games.add(new Game(teams.get(i*2), teams.get(i*2 + 1), gameIndex));
+            gameIndex++;
+        }
+        for (int i = 0; i < teams.size() / 2 - 1; i++) {
+            games.add(new Game(gameIndex));
+            gameIndex++;
+        }
+    }
 
-	static void computeWinners() {
-		ArrayList<Team> winners = new ArrayList<Team>();
-		for (int i = 0; i < teams.size() / 2; i++) {
-			int result = teams.get(2 * i).getGameScore() - teams.get(2 * i + 1).getGameScore();
-			if (result < 0)
-				winners.add(teams.get(2 * i + 1));
-			else if (result > 0)
-				winners.add(teams.get(2 * i));
-			else {
-				Alert alert = new Alert(AlertType.CONFIRMATION);
-				alert.setTitle("Confirmation Dialog");
-				alert.setHeaderText("Two teams have the same score!");
-				alert.setContentText("Choose your winning team of this game between\n" + "A: Team "
-						+ teams.get(2 * i).getName() + "\n and\n" + "B: Team " + teams.get(2 * i + 1).getName());
+    static void computeWinners() {
+        ArrayList<Team> winners = new ArrayList<Team>();
+        for (int i = 0; i < teams.size() / 2; i++) {
+            int result = teams.get(2 * i).getGameScore() - teams.get(2 * i + 1).getGameScore();
+            if (result < 0)
+                winners.add(teams.get(2 * i + 1));
+            else if (result > 0)
+                winners.add(teams.get(2 * i));
+            else {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation Dialog");
+                alert.setHeaderText("Two teams have the same score!");
+                alert.setContentText("Choose your winning team of this game between\n" + "A: Team "
+                        + teams.get(2 * i).getName() + "\n and\n" + "B: Team " + teams.get(2 * i + 1).getName());
 
-				ButtonType buttonTypeOne = new ButtonType("A");
-				ButtonType buttonTypeTwo = new ButtonType("B");
+                ButtonType buttonTypeOne = new ButtonType("A");
+                ButtonType buttonTypeTwo = new ButtonType("B");
 
-				alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo);
+                alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo);
 
-				Optional<ButtonType> choice = alert.showAndWait();
-				if (choice.get() == buttonTypeOne) {
-					winners.add(teams.get(2 * i));
-				} else {
-					winners.add(teams.get(2 * i + 1));
-				}
-			}
-		}
-		teams = winners;
-	}
+                Optional<ButtonType> choice = alert.showAndWait();
+                if (choice.get() == buttonTypeOne) {
+                    winners.add(teams.get(2 * i));
+                } else {
+                    winners.add(teams.get(2 * i + 1));
+                }
+            }
+        }
+        teams = winners;
+    }
 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			root = new BorderPane();
-			root = GUI.setupGUI(root);
-			Scene scene = new Scene(root, HEIGHT, WIDTH);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			stage.setScene(scene);
-			stage.setTitle("Tournament Bracket");
-			stage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            root = new BorderPane();
+            root = GUI.setupGUI(root);
+            root.getCenter().getStyleClass().add("borderpane");
+            Scene scene = new Scene(root, HEIGHT, WIDTH);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            stage.setScene(scene);
+            stage.setTitle("Tournament Bracket");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		if (args != null && args.length > 0)
-			loadDefaultFile(args[0]);
-		else
-			loadDefaultFile(null);
-		launch(args);
-	}
+    public static void main(String[] args) {
+        if (args != null && args.length > 0)
+            loadDefaultFile(args[0]);
+        else
+            loadDefaultFile(null);
+        launch(args);
+    }
 }
